5174d4d81c10d8966015b1820647174f58f9063a
Adrian Weaver <aweaver@student.42.fr>
2021-12-02 15:14:31 +0100

    fixing strchr and strrchr to suit both linux and macOs

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 09:29:31 by aweaver           #+#    #+#             */
-/*   Updated: 2021/12/02 09:36:17 by aweaver          ###   ########.fr       */
+/*   Updated: 2021/12/02 15:09:51 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ char	*ft_strchr(const char *s, int c)
 	int	i;
 
 	i = 0;
-	if (!ft_isascii(c))
-		return ((char *)s);
+	if (c >= 256)
+		c %= 256;
 	while (s[i])
 	{
 		if (s[i] == c)
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 10:14:26 by aweaver           #+#    #+#             */
-/*   Updated: 2021/12/02 09:39:03 by aweaver          ###   ########.fr       */
+/*   Updated: 2021/12/02 15:13:58 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ char	*ft_strrchr(const char *s, int c)
 {
 	int	i;
 
-	if (!ft_isascii(c))
-		return ((char *)s);
 	i = ft_strlen(s);
+	if (c >= 256)
+		c %= 256;
 	while (i >= 0)
 	{
 		if (s[i] == c)
