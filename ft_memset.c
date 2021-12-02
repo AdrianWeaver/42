e63ee95f0bee837b8e2cadd8962773fe3a73f1bd
Adrian Weaver <aweaver@student.42.fr>
2021-12-02 15:39:33 +0100

    removing mains and incorrect headers

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 13:43:35 by aweaver           #+#    #+#             */
-/*   Updated: 2021/11/26 12:18:37 by aweaver          ###   ########.fr       */
+/*   Updated: 2021/12/02 15:34:41 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,28 +24,3 @@ void	*ft_memset(void *s, int c, size_t n)
 	}
 	return (s);
 }
-
-/*
-#include <stdio.h>
-int	main(int ac, char** av)
-{
-	(void)ac;
-	(void)av;
-	size_t	n;
-	int		i;
-
-	i = 0;
-	n = 2;
-	char s1[]="toto"; 
-	char s2[]="toto";
-
-	ft_memset(s1, 'a', n);
-	memset(s2, 97, n);
-	printf("yes\n");
-	while (s2[i])
-	{
-		printf("%c\n", s2[i]);
-		i++;
-	}
-}
-*/
